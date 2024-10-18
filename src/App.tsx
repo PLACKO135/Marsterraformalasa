447e30e860b8f8edb37421caa4521b9a2159012c
Placko135 <papplaszlolevente@gmail.com>
2024-10-18 13:09:23 +0200

    óravége

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,101 @@
 import './app.css'
-import { useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import Resource from './components/Resource/Resorce'
 import Playername from './components/PlayerName/Playername'
 
-const App = () => {
+function App  ()  {
+  const loadmegac=()=>{
+    const savedmc=localStorage.getItem("megacredit");
+    return savedmc? Number(savedmc):0;
+  }
   const [Megacredit, setmegacredit] = useState(0)
   const [Megacreditprod, setmegacreditprod] = useState(0)
-  const handlecreditchange = (increment:number) => {
-    setmegacredit(Megacredit + increment)
-  }
-  const handlecreditprod = (increment:number) => {
-    setmegacreditprod(Megacreditprod + increment)
-  }
+ 
   const [Steel, setsteel] = useState(0)
   const [Steelprod, setsteelprod] = useState(0)
-  const handlesteelchange = (increment:number) => {
-    setsteel(Steel + increment)
-  }
-  const handlesteelchangeprod = (increment:number) => {
-    setsteelprod(Steelprod + increment)
-  }
+
   const [Titanite, setTitanite] = useState(0)
   const [Titaniteprod, setTitaniteprod] = useState(0)
-  const handletitanitechange = (increment:number) => {
-    setTitanite(Titanite + increment)
-  }
-  const handletitanitechangeprod = (increment:number) => {
-    setTitaniteprod(Titaniteprod + increment)
-  }
+
+  
   const [Herb, setherb] = useState(0)
   const [Herbprod, setherbprod] = useState(0)
-  const handleherbchange = (increment:number) => {
-    setherb(Herb + increment)
-  }
-  const handleherbchangeprod = (increment:number) => {
-    setherbprod(Herbprod + increment)
-  }
+
   const [Energy, setEnergy] = useState(0)
   const [Energyprod, setEnergyprod] = useState(0)
-  const handleEnergychange = (increment:number) => {
-    setEnergy(Energy + increment)
-  }
-  const handleEnergychangeprod = (increment:number) => {
-    setEnergyprod(Energyprod + increment)
-  }
+ 
   const [Heat, setHeat] = useState(0)
   const [Heatprod, setHeatprod] = useState(0)
-  const handleheatchange = (increment:number) => {
-    setHeat(Heat + increment)
-  }
-  const handleheatchangeprod = (increment:number) => {
-    setHeatprod(Heatprod + increment)
-  }
-
+ 
 
 const handleIcrement=(increment: number, resource:number, setResource: (vlaue:number)=>void)=>{
    setResource(resource+increment)
 }
 
+
+useEffect(()=>{
+  localStorage.setItem("megacredit", Megacredit.toString())
+  }, [Megacredit])
+
+
+const handlSubmit2= (e:FormEvent)=>{ 
+  e.preventDefault() 
+  setmegacredit(Megacredit+Megacreditprod)
+  setsteel(Steel+Steelprod)
+  setTitanite(Titanite+Titaniteprod)
+  setEnergy(Energyprod+Energy)
+  setherb(Herb+Herbprod)
+  setHeat(Heat+Heatprod)
+
+}
+
   return (
-    <div className="container">
+    <form onSubmit={handlSubmit2} className="container">
       <Playername/>
       <Resource 
         name='Megacredit' 
         amount={Megacredit} 
         onchange={(increment:number)=>handleIcrement(increment,Megacredit,setmegacredit)}
         production={Megacreditprod}
-        onchangeprod={handlecreditprod}
+        onchangeprod={(increment:number)=>handleIcrement(increment,Megacreditprod,setmegacreditprod)}
         />
          <Resource 
         name='Steel' 
         amount={Steel} 
-        onchange={handlesteelchange}
+        onchange={(increment:number)=>handleIcrement(increment,Steel,setsteel)}
         production={Steelprod}
-        onchangeprod={handlesteelchangeprod}
+        onchangeprod={(increment:number)=>handleIcrement(increment,Steelprod,setsteelprod)}
         />
           <Resource 
         name='Titanite' 
         amount={Titanite} 
-        onchange={handletitanitechange}
+        onchange={(increment:number)=>handleIcrement(increment,Titanite,setTitanite)}
         production={Titaniteprod}
-        onchangeprod={handletitanitechangeprod}
+        onchangeprod={(increment:number)=>handleIcrement(increment,Titaniteprod,setTitaniteprod)}
         />
           <Resource 
         name='Herb' 
         amount={Herb} 
-        onchange={handleherbchange}
+        onchange={(increment:number)=>handleIcrement(increment,Herb,setherb)}
         production={Herbprod}
-        onchangeprod={handleherbchangeprod}
+        onchangeprod={(increment:number)=>handleIcrement(increment,Herbprod,setherbprod)}
         />
-              <Resource name="Energy" production={Energyprod} amount={Energy} onchange={handleEnergychange} onchangeprod={handleEnergychangeprod}/>
-              <Resource name="Heat" production={Heatprod} amount={Heat} onchange={handleheatchange} onchangeprod={handleheatchangeprod}/>
+              <Resource 
+              name="Energy" 
+              production={Energyprod}
+              amount={Energy} 
+              onchange={(increment:number)=>handleIcrement(increment,Energy,setEnergy)} 
+              onchangeprod={(increment:number)=>handleIcrement(increment,Energyprod,setEnergyprod)}/>
+              <Resource 
+              name="Heat" 
+              production={Heatprod} 
+              amount={Heat} 
+              onchange={(increment:number)=>handleIcrement(increment,Heat,setHeat)} 
+              onchangeprod={(increment:number)=>handleIcrement(increment,Heatprod,setHeatprod)}/>
+
        <button type='submit' className='NextRound'>Next Round</button>
 
-    </div>
+    </form>
   )
 }
 
